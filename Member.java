48fbe8b904c37e1403d0dc2b491013a6e1a919b6
Marco Bang Romeri <33222584+Rephousa@users.noreply.github.com>
2017-11-21 11:07:45 +0100

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Member {
     private int ID;
@@ -14,9 +16,10 @@ public class Member {
     private boolean isMotionist;
     private boolean isCompetitor;
     private boolean isInArrears;
-    private BufferedReader bufferedReader;
+    private static BufferedReader bufferedReader;
     private Writer writer;
 
+    //Constructor for creating a new Member and save it to file
     public Member(String firstName, String lastName, String address, int zipcode, String city, long birthday, int telephoneNumber, boolean isActive, boolean isPassive, boolean isMotionist, boolean isCompetitor, boolean isInArrears) throws Exception {
         setID();
 
@@ -36,7 +39,56 @@ public class Member {
         saveNewMember();
     }
 
-    public void setID() throws Exception {
+    //Constuctor for recieving info about member from file
+    public Member(int ID) throws Exception {
+        this.ID = ID;
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(new File("member.dat")));
+
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                if(Integer.valueOf(line.substring(0, line.indexOf(" "))) == ID) {
+                    line = line.substring(line.indexOf(" ")+1, line.length());
+                    this.firstName = line.substring(0, line.indexOf(" "));
+                    line = line.substring(line.indexOf(" ")+1, line.length());
+                    this.lastName = line.substring(0, line.indexOf(" "));
+                    line = line.substring(line.indexOf(" ")+1, line.length());
+                    this.address = line.substring(0, line.indexOf(" "));
+                    line = line.substring(line.indexOf(" ")+1, line.length());
+                    this.zipcode = Integer.valueOf(line.substring(0, line.indexOf(" ")));
+                    line = line.substring(line.indexOf(" ")+1, line.length());
+                    this.city = line.substring(0, line.indexOf(" "));
+                    line = line.substring(line.indexOf(" ")+1, line.length());
+                    this.birthday = Long.valueOf(line.substring(0, line.indexOf(" ")));
+                    line = line.substring(line.indexOf(" ")+1, line.length());
+                    this.telephoneNumber = Integer.valueOf(line.substring(0, line.indexOf(" ")));
+                    line = line.substring(line.indexOf(" ")+1, line.length());
+                    this.isActive = Boolean.valueOf(line.substring(0, line.indexOf(" ")));
+                    line = line.substring(line.indexOf(" ")+1, line.length());
+                    this.isPassive = Boolean.valueOf(line.substring(0, line.indexOf(" ")));
+                    line = line.substring(line.indexOf(" ")+1, line.length());
+                    this.isMotionist = Boolean.valueOf(line.substring(0, line.indexOf(" ")));
+                    line = line.substring(line.indexOf(" ")+1, line.length());
+                    this.isCompetitor = Boolean.valueOf(line.substring(0, line.indexOf(" ")));
+                    line = line.substring(line.indexOf(" ")+1, line.length());
+                    this.isInArrears = Boolean.valueOf(line.substring(0, line.length()));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setID() throws Exception {
         try {
             bufferedReader = new BufferedReader(new FileReader(new File("member.dat")));
 
@@ -149,27 +201,46 @@ public class Member {
         isCompetitor = competitor;
     }
 
+    public void saveChanges() throws Exception {
+        String temp = "";
+
+        temp = getContentFromFile();
+
+        if(temp.length() != 0) {
+            try {
+                writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("member.dat")), "UTF8"));
+
+                while (temp.charAt(0) == '#') {
+                    temp = temp.substring(1, temp.length());
+
+                    if(Integer.valueOf(temp.substring(0, temp.indexOf(" "))) == ID) {
+                        writer.write(ID+" "+firstName+" "+lastName+" "+address.replace(" ", "_")+" "+zipcode+" "+city+" "+birthday+" "+telephoneNumber+" "+isActive+" "+isPassive+" "+isMotionist+" "+isCompetitor+" "+isInArrears);
+                    } else {
+                        writer.write(temp.indexOf("#") >= 0 ? temp.substring(0, temp.indexOf("#")) + "\n" : temp.substring(0, temp.length()) + "\n");
+                    }
+
+                    temp = temp.substring(temp.indexOf("#") < 0 ? 0 : temp.indexOf("#"), temp.length());
+                }
+
+                writer.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (writer != null) {
+                        writer.close();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     public void saveNewMember() throws Exception {
         String temp = "";
 
-        try {
-            bufferedReader = new BufferedReader(new FileReader(new File("member.dat")));
-
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                temp += "#" + line;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if(bufferedReader != null) {
-                    bufferedReader.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        temp = getContentFromFile();
 
         if(temp.length() != 0) {
             try {
@@ -197,5 +268,121 @@ public class Member {
                 }
             }
         }
+    }
+
+    public static int countMember() {
+        int returnValue = 0;
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(new File("member.dat")));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                returnValue++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return returnValue;
+    }
+
+    public static String[] getMemberForRow(int row) {
+        String[] returnArray = new String[10];
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(new File("member.dat")));
+
+            String line;
+            int rowNum = 1;
+            while ((line = bufferedReader.readLine()) != null) {
+                if(rowNum == row) {
+                    for (int i = 0; i < returnArray.length; i++) {
+                        String temp = line.substring(0, line.indexOf(" ") >= 0 ? line.indexOf(" ") : line.length());
+
+                        if(i == 3) {
+                            returnArray[i] = String.valueOf(temp).replace("_", " ");
+                        } else if(i == 6) {
+                            returnArray[i] = String.valueOf(new SimpleDateFormat("dd-MM-yyyy").format(new Date(Long.valueOf(temp))));
+                        } else if(i == 8) {
+                            returnArray[i] = temp.equals("true") ? "AKTIV" : "PASSIV";
+                        } else if(i == 9) {
+                            returnArray[i] = temp.equals("true") ? "MOTIONIST" : "KONKURRENCE";
+                        } else {
+                            returnArray[i] = String.valueOf(temp);
+                        }
+
+                        line = line.substring(line.indexOf(" ") >= 0 ? line.indexOf(" ")+1 : line.length(), line.length());
+                    }
+                }
+
+                rowNum++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return returnArray;
+    }
+
+    public static boolean doesMemberExist(int ID) {
+        boolean returnValue = false;
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(new File("member.dat")));
+
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                if(Integer.valueOf(line.substring(0, line.indexOf(" "))) == ID) {
+                    returnValue = true;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return returnValue;
+    }
+
+    public String getContentFromFile() {
+        String returnValue = "";
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(new File("member.dat")));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                returnValue += "#" + line;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return returnValue;
     }
 }
